bb241f7383f7266fb0827cabd4153363727f0e3a
Michael Earls <me@cerkit.com>
2025-12-18 19:33:11 -0600

    Initial Commit

---
+++ Retro180Emulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Retro180Emulator
+//
+//  Created by Michael Earls on 12/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Retro180Emulator/Retro180EmulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Retro180EmulatorApp.swift
+//  Retro180Emulator
+//
+//  Created by Michael Earls on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Retro180EmulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
